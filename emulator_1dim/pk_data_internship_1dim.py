f3fe0c81d4262123446da3c67811d120f5a03b8d
Mehdi Noor <100883809+MSN64@users.noreply.github.com>
2022-06-07 18:09:10 +0200

    Add files via upload

--- emulator_1dim/pk_data_internship_1dim.py
+++ emulator_1dim/pk_data_internship_1dim.py
@@ -28,16 +28,19 @@ cosmo_params[:, 0] = train_params['h']
 
 def camb_cosmo(i):
     pars = camb.CAMBparams()
-    pars.set_cosmology(H0=100 * cosmo_params[i, 0], ombh2=0.022, omch2=0.122)
-    pars.InitPower.set_params(As=2e-9, ns=0.965)
-    pars.set_matter_power(redshifts=[0.0], kmax=10.0)
+    pars.set_cosmology(H0=100 * cosmo_params[i, 0], ombh2=0.02242, omch2=0.11933)
+    pars.InitPower.set_params(As=2.105209331e-9, ns=0.9665)
+    pars.set_matter_power(redshifts=[0.0], kmax=15.0)
     pars.set_dark_energy(w=-1.0, wa=0, dark_energy_model='fluid')
 
     # Linear spectra
     pars.NonLinear = model.NonLinear_none
     results = camb.get_results(pars)
-    camb_kh, z, camb_pk = results.get_matter_power_spectrum(minkh=1e-4, maxkh=15, npoints=2000)
-    s8 = np.array(results.get_sigma8())
+    camb_kh, z, camb_pk_cur = results.get_matter_power_spectrum(minkh=1e-4, maxkh=15, npoints=500)
+    s8_current = np.array(results.get_sigma8())
+    s8_fid = np.array([0.8102])
+    renorm_s8 = (s8_fid/s8_current)**2
+    camb_pk = renorm_s8*camb_pk_cur
     # s8 is the amplitude of matter fluctuations, i.e. in term of the average r.m.s fluctuation in a sphere of 8h^{−1}Mpc
 
     return camb_kh, camb_pk[0]
@@ -59,10 +62,10 @@ for i in range(len(train_params['h'])):
                cosmo_func = camb_cosmo(i)
                pk_matrix.append(cosmo_func[1])
 
-with open('pk_data_1dim5000.pkl', 'wb') as f:
+with open('pk_data_1dim5000_np500v2.pkl', 'wb') as f:
     pickle.dump(pk_matrix, f)
 
-with open('pk_data_1dim5000.pkl', 'rb') as f:
+with open('pk_data_1dim5000_np500v2.pkl', 'rb') as f:
     pk_matrix = pickle.load(f)
 
 print(np.shape(pk_matrix))
